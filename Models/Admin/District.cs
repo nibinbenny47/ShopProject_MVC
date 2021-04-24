022a45eebea43d949b310ee250e58f24001d1312
Nibin Benny <nibinbenny47@gmail.com>
2021-04-24 14:05:10 +0530

    Add project files.

---
+++ Models/Admin/District.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopProject_MVC.Models.Admin
+{
+    public class District
+    {
+        public int district_id { get; set; }
+        public string disṭrict_name { get; set; }
+    }
+}
