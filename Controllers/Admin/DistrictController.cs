e3f61265290b7e9413279b6b696cd6005ca634e1
Nibin Benny <nibinbenny47@gmail.com>
2021-04-24 14:24:20 +0530

    distrcit names inserted

---
+++ Controllers/Admin/DistrictController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShopProject_MVC.Models.Admin;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ShopProject_MVC.Controllers.Admin
+{
+    public class DistrictController : Controller
+    {
+        SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=db_MVCProject;Integrated Security=True");
+
+        // GET: District
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult insertDistrict()
+        {
+            District obj = new District();
+            return View(obj);
+        }
+        [HttpPost]
+        public ActionResult insertDistrict(District obj)
+        {
+            string insQry = "insert into tbl_district(district_name) values('" + obj.disṭrict_name + "')";
+            SqlCommand cmd = new SqlCommand(insQry, con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            ModelState.Clear();
+            return View(obj);
+
+
+        }
+    }
+}
