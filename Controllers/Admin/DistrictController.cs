e1bce168f08abed878f25d5c9f03653a8a03ac05
Nibin Benny <nibinbenny47@gmail.com>
2021-04-24 19:51:31 +0530

    Edit and delete done in DistrictController

--- Controllers/Admin/DistrictController.cs
+++ Controllers/Admin/DistrictController.cs
@@ -37,5 +37,61 @@ namespace ShopProject_MVC.Controllers.Admin
 
 
         }
+        //---------show district names---------------
+        public ActionResult displayDistrict()
+        {
+            string selQry = "select * from tbl_district";
+            var model = new List<District>();
+            SqlCommand cmd = new SqlCommand(selQry, con);
+            con.Open();
+            SqlDataReader rdr = cmd.ExecuteReader();
+            
+            while (rdr.Read())
+            {
+                var obj = new District();
+                obj.district_id = (int)rdr["district_id"];
+                obj.disṭrict_name = (string)rdr["district_name"];
+                model.Add(obj);
+
+            }
+            return View(model);
+        }
+        //-----------delete district -----------
+        public ActionResult deleteDistrict(int id)
+        {
+            string delQry = "delete from tbl_district where district_id='"+id+"'";
+            SqlCommand cmd = new SqlCommand(delQry, con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            return RedirectToAction("displayDistrict");
+        }
+        //----------edit district----------------
+        public ActionResult updateDistrict(int id,District obj)
+        {
+            string selQry = "select * from tbl_district where district_id ='"+id+"'";
+            SqlCommand cmd = new SqlCommand(selQry, con);
+            con.Open();
+            SqlDataReader rdr = cmd.ExecuteReader();
+           // District obj = new District();
+            while (rdr.Read())
+            {
+                
+                obj.district_id = (int)rdr["district_id"];
+                obj.disṭrict_name = (string)rdr["district_name"];
+            }
+            return View();
+        }
+        [HttpPost]
+        public ActionResult updateDistrict(District obj,int id)
+        {
+            string upQry = "update tbl_district set district_name='"+obj.disṭrict_name+"' where district_id='"+id+"'";
+            SqlCommand cmd = new SqlCommand(upQry, con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            return View(obj);
+        }
+
     }
 }
