6e055c1e53ddd9ad8ef75eab68f9832f25a789f9
Nibin Benny <nibinbenny47@gmail.com>
2021-04-27 16:26:33 +0530

    Admin Viewing New Shop List

---
+++ Controllers/Admin/ViewNewShopListController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using ShopProject_MVC.Models.Admin;
+namespace ShopProject_MVC.Controllers.Admin
+{
+    public class ViewNewShopListController : Controller
+    {
+        SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=db_MVCProject;Integrated Security=True");
+
+        // GET: ViewNewShopList
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //-------display all new shop list--------------------------------------------------
+        public ActionResult displayShopList()
+        {
+            string selQry = "select * from tbl_shop s inner join tbl_place p on s.place_id=p.place_id inner join tbl_subcategory sc on sc.subcategory_id=s.subcategory_id where s.shop_status =0";
+            SqlCommand cmd = new SqlCommand(selQry, con);
+            con.Open();
+            SqlDataReader rdr = cmd.ExecuteReader();
+            var model = new List<ViewNewShopList>();
+            while (rdr.Read())
+            {
+                var obj = new ViewNewShopList();
+                obj.shop_name = rdr["shop_name"].ToString();
+                obj.shop_email = rdr["shop_email"].ToString();
+                obj.shop_contact = rdr["shop_contact"].ToString();
+                obj.shop_address = rdr["shop_address"].ToString();
+                obj.imagepath = rdr["shop_photo"].ToString();
+                obj.ddlPlace = rdr["place_name"].ToString();
+                obj.ddlSubcategory = rdr["subcategory_name"].ToString();
+                model.Add(obj);
+
+
+            }
+            return View(model);
+
+        }
+    }
+}
---
+++ Models/Admin/ViewNewShopList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShopProject_MVC.Models.Admin
+{
+    public class ViewNewShopList
+    {
+        public int shop_id { get; set; }
+        public string shop_name { get; set; }
+        public string shop_address { get; set; }
+        public string shop_contact { get; set; }
+        public string shop_email { get; set; }
+        public string shop_photo { get; set; }
+        public string shop_username { get; set; }
+        public string shop_password { get; set; }
+        public string shop_status { get; set; }
+        //------for file upload---------------
+        public string imagepath { get; set; }
+        public HttpPostedFileBase imagefile { get; set; }
+        //-------for district dropdown------------
+        public List<SelectListItem> districtList { get; set; }
+        public string ddlDistrict { get; set; }
+        //-------for place dropdown------------
+        public List<SelectListItem> placeList { get; set; }
+        public string ddlPlace { get; set; }
+        //-------for category dropdown-----------
+        public List<SelectListItem> categoryList { get; set; }
+
+        public string ddlCategory { get; set; }
+        //-----for subcategory dropdown-------------
+        public List<SelectListItem> subcategoryList { get; set; }
+        public string ddlSubcategory { get; set; }
+    }
+}
